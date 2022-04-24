10be4d62f67f47904950506e1effda793bb156a8
eldegen <eliyaz2761@gmail.com>
2022-04-24 20:03:34 +0600

    Cleaned up the code

--- app/src/main/java/com/example/hw4k1/adapters/PagerAdapter.kt
+++ app/src/main/java/com/example/hw4k1/adapters/PagerAdapter.kt
@@ -2,13 +2,10 @@ package com.example.hw4k1.adapters
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.hw4k1.ui.fragments.main.MainFragment
 import com.example.hw4k1.ui.fragments.second.SecondFragment
 import com.example.hw4k1.ui.fragments.third.ThirdFragment
-
 
 class PagerAdapter(fa: FragmentActivity) :   FragmentStateAdapter(fa) {
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/hw4k1/ui/activities/main/MainActivity.kt
+++ app/src/main/java/com/example/hw4k1/ui/activities/main/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun initView() {
         adapter = PagerAdapter(this)
 
-
         binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         binding.viewPager.adapter = this.adapter
     }
@@ -31,11 +30,3 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
 }
-
-/*
-- Добавить ViewPager из 3 фрагментов в MainActivity
-- 1 фрагмент кнопки ‘+’ и ‘-’, при нажатии вызывать соответствующие методы у ViewModel и выполнять необходимую логику
-- 2 фрагмент отображает текущее значение LiveData счетчика
-- 3 фрагмент отображает список выполненных операций (просто список строковых значений, который наблюдаются у ViewModel-a)
-- Для всех задач использовать один ViewModel
-*/
